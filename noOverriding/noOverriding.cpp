deb88ec437ffc33f48b8461197cc133b2fea74bf
NurulNajatMony <nurulmony505@gmail.com>
2025-06-02 11:36:16 +0700

    menambahkan baseClass dengan method final perkenalan()

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+public :
+    virtual void perkenalan() final {
+        cout << "Hallo saya function dari base class";
+    }
+};
+class derivedClass : public baseClass {
+public :
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
