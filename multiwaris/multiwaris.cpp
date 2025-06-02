9095b3eacc75780dd1dcaa385204f941876f4866
NurulNajatMony <nurulmony505@gmail.com>
2025-06-02 11:13:28 +0700

    menambahkan  class pelajar dengan inheritance virtual dari orang

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -21,4 +21,14 @@ public :
     }
 };
 
+class pelajar : virtual public orang {
+public :
+
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
 
